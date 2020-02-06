adb113c682c853bd477667681e642ca7524179e0
joekrom <joel.dfankam@yahoo.fr>
2020-02-06 08:28:01 +0100

    second version

--- registration.py
+++ registration.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "that is the service for registration first version herve likes energy drink"
+    return "that is the service for registration first version herve likes energy drink,first modif to github"
 
 if __name__== "__main__":
     app.run(host='0.0.0.0',debug=True )
